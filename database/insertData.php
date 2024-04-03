67f637e14e0f5e40801cf28888fad428c0a9b6e2
aihki <laine.nestori.julius@gmail.com>
2024-04-03 14:50:22 +0300

    modify

--- database/insertData.php
+++ database/insertData.php
@@ -1,16 +1,33 @@
 <?php
+session_start();
+if (!isset($_SESSION['user'])) {
+    header('Location: index.php');
+    exit;
+}
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 global $DBH;
 require 'dbConnect.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['title']) && isset($_POST['description'])) {
+    if (isset($_POST['title']) && isset($_POST['description']) && $_FILES['file'] !== null) {
+        $filename = $_FILES['file']['name'];
+        $filetype = $_FILES['file']['type'];
+        $filesize = $_FILES['file']['size'];
+        $temp_file = $_FILES['file']['tmp_name'];
+        $destination = __DIR__ . '/uploads/' . $filename;
+        if (!move_uploaded_file($temp_file, $destination)) {
+             header('Location: home.php?success=File upload failed');
+            exit;
+        }
+
         $data = [
-            'user_id' => 1,
-            'filename' => 'https://placekitten.com/640',
-            'media_type' => 'image/jpeg',
+            'user_id' => $_SESSION['user']['user_id'],
+            'filename' => $filename,
+            'media_type' => $filetype,
             'title' => $_POST['title'],
             'description' => $_POST['description'],
-            'filesize' => 1234,
+            'filesize' => $filesize,
         ];
 
         $sql = 'INSERT INTO MediaItems (user_id, filename, filesize, media_type, title, description) 
@@ -19,8 +36,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         try {
             $STH = $DBH->prepare($sql);
             $STH->execute($data);
-            header('Location: index.php?success=Item added');
-        } catch (PDOException $e){
+            header('Location: home.php?success=Item added');
+        } catch (PDOException $e) {
             echo "Could not insert data into the database.";
             file_put_contents('PDOErrors.txt', 'insertData.php - ' . $e->getMessage(), FILE_APPEND);
         }
--- database/modifyData.php
+++ database/modifyData.php
@@ -1,6 +1,6 @@
 <?php
 global $DBH;
-require 'dbConnect.php';
+require_once 'dbConnect.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['title']) && isset($_POST['description'])) {
@@ -16,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         try {
             $STH = $DBH->prepare($sql);
             $STH->execute($data);
-            header('Location: index.php?success=Item added');
+            header('Location: home.php?success=Item added');
         } catch (PDOException $e){
             echo "Could not modify data into the database.";
             file_put_contents('PDOErrors.txt', 'modifyData.php - ' . $e->getMessage(), FILE_APPEND);
